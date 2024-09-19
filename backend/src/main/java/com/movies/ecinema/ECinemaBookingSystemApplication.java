5563bb931c65ce20f4b68cbca0ff07f7da037d18
nernoel <142674871+nernoel@users.noreply.github.com.>
2024-09-18 20:08:05 -0400

    Initial commit

---
+++ backend/src/main/java/com/movies/ecinema/ECinemaBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.movies.ecinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECinemaBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECinemaBookingSystemApplication.class, args);
+	}
+
+}
