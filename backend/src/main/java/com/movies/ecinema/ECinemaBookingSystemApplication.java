bfc86fae49d29f50554de185677bc186296dda59
nernoel <142674871+nernoel@users.noreply.github.com.>
2024-09-20 18:46:10 -0400

    "Movie entity backend working"

--- backend/src/main/java/com/movies/ecinema/ECinemaBookingSystemApplication.java
+++ backend/src/main/java/com/movies/ecinema/ECinemaBookingSystemApplication.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Bean;
 // Importing model mapper
 import org.modelmapper.ModelMapper;
 
+// Importing environment variable loader
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ECinemaBookingSystemApplication {
 
@@ -20,7 +23,11 @@ public class ECinemaBookingSystemApplication {
 
 	// Main function to run the application
 	public static void main(String[] args) {
-
+		// Loading environment variables
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
 		// Running the spring application
 		SpringApplication.run(ECinemaBookingSystemApplication.class, args);
